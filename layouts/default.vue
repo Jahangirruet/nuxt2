6cca42543ee77ce67e0dad1cecc82884f8b4bc32
Md. Jahangir Alam <jahangir.2012@outlook.com>
2023-04-28 05:32:47 +0600

    nuxt2.1
    nuxt2.1

---
+++ layouts/default.vue
@@ -0,0 +1,48 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div>
+    <nav class="navbar">
+      <ul class="nav-list">
+        <li class="nav-item"><a href="#home">Home</a></li>
+        <li class="nav-item"><a href="#about">About</a></li>
+        <li class="nav-item"><a href="#services">Services</a></li>
+        <li class="nav-item"><a href="#portfolio">Portfolio</a></li>
+        <li class="nav-item"><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+
+    <nuxt />
+  </div>
+</template>
+
+<style>
+.navbar {
+  background-color: #333; /* Navbar background color */
+  height: 60px; /* Navbar height */
+}
+
+.nav-list {
+  display: flex; /* Display list items in a row */
+  justify-content: center; /* Center align the list items */
+  align-items: center; /* Vertically align the list items */
+  height: 100%; /* Set list items height to 100% of navbar */
+  list-style-type: none; /* Remove default list styles */
+  margin: 0; /* Remove default margin */
+  padding: 0; /* Remove default padding */
+}
+
+.nav-item {
+  margin-right: 20px; /* Add spacing between list items */
+}
+
+.nav-item a {
+  color: #fff; /* Link text color */
+  text-decoration: none; /* Remove underline */
+  transition: color 0.3s ease; /* Add smooth transition to link color */
+}
+
+.nav-item a:hover {
+  color: #ff9800; /* Change link color on hover */
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+    <!-- <TheNavBar /> -->
+    <h1>Home page</h1>
+    <NuxtLink to="/about">
+      About (internal link that belongs to the Nuxt App)
+    </NuxtLink>
+    <a href="https://nuxtjs.org">External Link to another page</a>
+  </main>
+</template>
+
+<script scoped>
+// import TheNavBar from '~/components/TheNavBar.vue'
+// export default {
+//   components: TheNavBar,
+// }
+</script>
